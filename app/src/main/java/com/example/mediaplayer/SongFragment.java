84400ade94c875a4dab9e56c8666f3cdde234828
nguyencaobach123 <57450696+nguyencaobach123@users.noreply.github.com>
2022-03-30 03:33:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mediaplayer/SongFragment.java
@@ -0,0 +1,38 @@
+package com.example.mediaplayer;
+
+import static com.example.mediaplayer.MainActivity.musicFiles;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SongFragment extends Fragment {
+    RecyclerView recyclerView;
+    MusicAdapter musicAdapter;
+    public SongFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_song, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        Log.e("Size", String.valueOf(musicFiles.size()));
+        if (musicFiles.size() >= 1) {
+            musicAdapter = new MusicAdapter(getContext(), musicFiles);
+            recyclerView.setAdapter(musicAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        }
+        return view;
+    }
+}
